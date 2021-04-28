5edebe3e7998c9bfa661ce288d4b892d08c7c0ad
unknown <longbestlee1909@gmail.com>
2021-04-28 16:03:32 +0700

    modify ok

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -5,30 +5,40 @@ const { LikeSchema } = require('./like.model')
 const { DislikeSchema } = require('./dislike.model')
 
 const PostSchema = new Schema({
-    userId: {
-        type: String,
-        default: null,
+    owner: {
+        type: Schema.Types.ObjectId,    //ng viet bai
+        ref: "User"
     },
     content: {
-        type: String,
+        type: String,                   //noi dung
         default: null,
     },
-    comments: [{
-        type : Schema.Types.ObjectId,
-		ref : "Comment",
-    }],
-    likes: [{
-        type : Schema.Types.ObjectId,
-		ref : "Like",
+    // comments: [{
+    //     type : Schema.Types.ObjectId,   //danh sach cac comment
+	// 	ref : "Comment",
+    // }],
+    likes: [{ 
+        type : Schema.Types.ObjectId,   //danh sach user like
+		ref : "User",
     }],
     dislikes: [{
-        type : Schema.Types.ObjectId,
-		ref : "Dislike",
+        type : Schema.Types.ObjectId,   //danh sach user dislike
+		ref : "User",
     }],
-    timeStamp: {
+    timeStamp: {                        //thoi gian
         type: Date,
         default: Date.now()
     },
+    image: [String],                    //danh sach anh.
+    videoEmbed: {                       //danh sach video youtube
+        type: String,
+        default: null
+    },
+    belongToGroup : {                        // thuoc ve group nao, neu null thi thuoc ve user profile
+        type: Schema.Types.ObjectId,
+        ref: "Group",
+        default: null
+    }
 });
 
 const Post = model("Post", PostSchema, "posts");
